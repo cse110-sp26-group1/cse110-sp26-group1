20e5b5be532fb21dfe3ca4d67beee54087330c46
Anchita-455 <70964157+Anchita-455@users.noreply.github.com>
2026-05-11 13:39:23 -0700

    ci: added changelog and JSDoc check

--- eslint.config.js
+++ eslint.config.js
@@ -1,29 +1,52 @@
 import globals from 'globals';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import jsdoc from 'eslint-plugin-jsdoc';
+
+import globals from 'globals';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import jsdoc from 'eslint-plugin-jsdoc';
 
 export default [
-	{
-		files: ['**/*.js'],
-		languageOptions: {
-			ecmaVersion: 'latest',
-			sourceType: 'module',
-			globals: {
-				...globals.browser,
-				...globals.node,
-			},
-		},
-		rules: {
-			'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-			'no-undef': 'error',
-			'no-console': 'warn',
-			eqeqeq: 'error',
-			curly: 'error',
-			'no-var': 'error',
-			'prefer-const': 'warn',
-		},
-	},
-	{
-		ignores: ['node_modules/', 'dist/', 'build/', 'coverage/', '.wrangler/', 'research/'],
-	},
-	eslintConfigPrettier,
+    {
+        files: ['**/*.js'],
+        plugins: {        
+            jsdoc,
+        },
+        languageOptions: {
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+        },
+        rules: {
+            'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+            'no-undef': 'error',
+            'no-console': 'warn',
+            eqeqeq: 'error',
+            curly: 'error',
+            'no-var': 'error',
+            'prefer-const': 'warn',
+
+            'jsdoc/require-jsdoc': ['warn', {
+                require: {
+                    FunctionDeclaration: true,
+                    MethodDefinition: true,
+                    ClassDeclaration: true,
+                    ArrowFunctionExpression: false,
+                },
+            }],
+            'jsdoc/require-param': 'warn',
+            'jsdoc/require-param-description': 'warn',
+            'jsdoc/require-param-type': 'warn',
+            'jsdoc/require-returns': 'warn',
+            'jsdoc/check-param-names': 'error',
+            'jsdoc/check-types': 'error',
+        },
+    },
+    {
+        ignores: ['node_modules/', 'dist/', 'build/', 'coverage/', '.wrangler/', 'research/'],
+    },
+    eslintConfigPrettier,
 ];
